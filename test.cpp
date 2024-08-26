0f2b4b528cca44e0ff629d146c050347fcd8e357
Guilherme Oliveira de Campos <oliveira.gui@hotmail.com.br>
2024-08-26 19:07:55 -0300

    first commit

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void mensagem(){
+    std::cout<<"Hello World!"<<std::endl;
+}
+
+int main(){
+    std::cout<<"O conteúdo da mensagem é: ";
+    mensagem();
+    return 0;
+    
+}
